a05d17a533064b1166a43e7d1896ae8b488a2991
Racheal Muchiri <rachealloveo6@gmail.com>
2023-08-16 15:16:48 +0300

    Update index.js
    conversion of units

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function formatDate() {
-  let currentTime = new Date();  
+
+  function formatDate() {
+  let currentTime = new Date();
 
   let currenthour = currentTime.getHours();
   if (currenthour < 10) {
     currenthour = `0${currenthour}`;
   }
-  let currentminute = currentTime.getMinutes();                                             
+  let currentminute = currentTime.getMinutes();
   if (currentminute < 10) {
     currentminute = `0${currentminute}`;
   }
@@ -19,8 +20,9 @@ formatDate();
 
 /* changing the city name*/
 function search(city) {
-  let apiKey = "05196328f3a63c6924aa5222ffc3236b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`; //
+  let apiKey = "aac97fb2fbt9362853a0a43aca162o74";
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`; //
   axios.get(apiUrl).then(showTemperature);
 }
 
@@ -34,53 +36,78 @@ let form = document.querySelector("#search-form");
 
 form.addEventListener("submit", handleSubmit);
 
-/*function click(event) {
+function showCelsiusTemp(event) {
   event.preventDefault;
-  let changecelsius = document.querySelector("#celsius");
-  changecelsius.innerHTML = "24"; 
+  let celsiusTemp = document.querySelector("#celsius");
+  clickcelcius.classList.add("active");
+  clickfaren.classList.remove("active");
+  celsiusTemp.innerHTML = Math.round(celsiusTemperature);
 }
 
-function faren(event) {
+function showFahrenheitTemp(event) {
   event.preventDefault();
-  let changefaren = document.querySelector("#celsius");
-  changefaren.innerHTML = `76`;
+  let celsiusTemp = document.querySelector("#celsius");
+  //remove class from celcuis link//
+  clickcelcius.classList.remove("active");
+  clickfaren.classList.add("active");
+  let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32;
+  celsiusTemp.innerHTML = Math.round(fahrenheitTemp);
 }
 
 let clickcelcius = document.querySelector("#celcius-link");
-clickcelcius.addEventListener("click", click);
+clickcelcius.addEventListener("click", showCelsiusTemp);
+
 let clickfaren = document.querySelector("#faren-link");
-clickfaren.addEventListener("click", faren);*/
+clickfaren.addEventListener("click", showFahrenheitTemp);
 
 /*api sec*/
 
 function showTemperature(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#celsius").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  document.querySelector("#city").innerHTML = response.data.city;
+
+  document.querySelector("#celsius").innerHTML = celsiusTemperature;
+
+  celsiusTemperature = Math.round(response.data.temperature.current);
+
   document.querySelector(
     "#humidity"
   ).innerHTML = `Humidity:${response.data.main.humidity}%`;
+
   document.querySelector("#wind").innerHTML = `Wind:${Math.round(
-    response.data.wind.speed
+    response.data.temperature.humidity
   )}km/hr`;
-  document.querySelector("#cloudy").innerHTML = response.data.weather[0].main;
+
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+
+  document
+    .querySelector("#weather-icon")
+    .setAttribute(
+      "src",
+      `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon_url}`
+    );
+  document
+    .querySelector("#weather-icon")
+    .setAttribute("alt", response.data.condition.description);
 }
 function searchPosition(position) {
-  let apiKey = "05196328f3a63c6924aa5222ffc3236b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`; //
+  let apiKey = "aac97fb2fbt9362853a0a43aca162o74";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${position.coordinates.longitude}&lat=${position.coordinates.latitude}&key=${apiKey}&units=metric`; //
   axios.get(apiUrl).then(showTemperature);
 }
 function getCurrentTemp(event) {
-  event.preventDefault;
+  event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchPosition);
 }
 
 let currentLocationButton = document.querySelector("#current");
 currentLocationButton.addEventListener("click", getCurrentTemp);
 
+let celsiusTemperature = null;
+
 search("Nairobi");
-  
+
+
   
   
 
